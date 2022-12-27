a506bcd8ecef389e5cb3d090946ac3198e5f27f5
Epsylon <eepsylon.3@gmail.com>
2022-12-27 22:48:01 +0200

    Remove custom js

--- static/mod.js
+++ static/mod.js
@@ -102,22 +102,6 @@ const mspfacomponent = {
 
             this.rangeClass = range;
         },
-
-        updateOverlay() {
-            const overlay = document.querySelector('#overlay');
-            if (overlay) {
-                let overlayAmt = parseInt(overlay.getAttribute('data-amt'));
-                if (isNaN(overlayAmt)) overlayAmt = 1;
-                for (let i = 0; i < overlayAmt; i++) {
-                    const newChild = document.createElement('div');
-                    newChild.className = `overlay-${i+1}`;
-                    overlay.appendChild(newChild);
-                }
-                document.querySelector('#container').appendChild(overlay);
-            }
-
-            //console.log("Image Overlay by seymour schlong -https://mspfa.com/?s=37172");
-        }
     },
 
     mounted() {
@@ -184,14 +168,11 @@ const mspfacomponent = {
                     break;
             }
         });
-
-        this.updateOverlay();
     },
 
     updated() {
         this.setButtonEvents();
         this.updateRangeClass();
-        this.updateOverlay();
     },
 }
 
