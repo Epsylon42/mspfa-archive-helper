bdcb1972b9ba210d397b9800c887667b0a86092b
Epsylon <eepsylon.3@gmail.com>
2022-12-27 22:48:41 +0200

    Add overlay JS from https://mspfa.com/?s=37172&p=1

--- static/mod.js
+++ static/mod.js
@@ -102,6 +102,22 @@ const mspfacomponent = {
 
             this.rangeClass = range;
         },
+
+        updateOverlay() {
+            const overlay = document.querySelector('#overlay');
+            if (overlay) {
+                let overlayAmt = parseInt(overlay.getAttribute('data-amt'));
+                if (isNaN(overlayAmt)) overlayAmt = 1;
+                for (let i = 0; i < overlayAmt; i++) {
+                    const newChild = document.createElement('div');
+                    newChild.className = `overlay-${i+1}`;
+                    overlay.appendChild(newChild);
+                }
+                document.querySelector('#container').appendChild(overlay);
+            }
+
+            //console.log("Image Overlay by seymour schlong -https://mspfa.com/?s=37172");
+        }
     },
 
     mounted() {
@@ -167,11 +183,14 @@ const mspfacomponent = {
                     break;
             }
         });
+
+        this.updateOverlay();
     },
 
     updated() {
         this.setButtonEvents();
         this.updateRangeClass();
+        this.updateOverlay();
     },
 }
 
